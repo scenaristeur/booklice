0b4903fb8859912af4afd8cb9b0b4ec378741da1
Scenaristeur <scenaristeur@gmail.com>
2021-06-28 00:31:18 +0200

    newNote 404-3

--- precache-manifest.6ba98999fc426bf06684a023d72b5d29.js
+++ precache-manifest.6ba98999fc426bf06684a023d72b5d29.js
@@ -4,7 +4,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/booklice/404.html"
   },
   {
-    "revision": "12baac6cc5ee02e497f4",
+    "revision": "e95ff8191a1225f33115",
     "url": "/booklice/css/app.5c9713c3.css"
   },
   {
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/booklice/css/chunk-vendors.590a9ee0.css"
   },
   {
-    "revision": "217b6d7e9550eade404dfc1b3d178357",
+    "revision": "40c51891b59743bd9dff52d7331d68b4",
     "url": "/booklice/index.html"
   },
   {
@@ -24,20 +24,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/booklice/js/about.229c6b90.js"
   },
   {
-    "revision": "12baac6cc5ee02e497f4",
-    "url": "/booklice/js/app.1b34b6df.js"
+    "revision": "e95ff8191a1225f33115",
+    "url": "/booklice/js/app.2c681d3b.js"
   },
   {
-    "revision": "801055ddb733a30f4966",
-    "url": "/booklice/js/bookmarks.aa00d546.js"
+    "revision": "38e8cd2640b6b55f8c96",
+    "url": "/booklice/js/bookmarks.4170fa89.js"
+  },
+  {
+    "revision": "8ba07f6950d9035d0564",
+    "url": "/booklice/js/chunk-2bfef5ad.be88ec00.js"
   },
   {
     "revision": "8732a94f4142a8791f77",
     "url": "/booklice/js/chunk-2d0bceed.93b618e8.js"
-  },
-  {
-    "revision": "336509131c5d66ab1d2a",
-    "url": "/booklice/js/chunk-2d0c8892.42b3a74e.js"
   },
   {
     "revision": "857dc60d9ab02ff20b17",
@@ -48,8 +48,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/booklice/js/chunk-2d21ed65.80f8fdc7.js"
   },
   {
-    "revision": "d7c708b79fd01e52cb1c",
-    "url": "/booklice/js/chunk-4e6e562f.013d8ad3.js"
+    "revision": "2a3ec70aede01d3d3c1e",
+    "url": "/booklice/js/chunk-392ccdba.e9ba4997.js"
   },
   {
     "revision": "e024c62336bfe3e16564",
