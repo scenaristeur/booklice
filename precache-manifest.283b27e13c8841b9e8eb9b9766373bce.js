b04d7cf380dbbba0cab648afbc474c042419f99e
Scenaristeur <scenaristeur@gmail.com>
2021-06-29 01:00:48 +0200

    screenshot+

---
+++ precache-manifest.283b27e13c8841b9e8eb9b9766373bce.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "937838ba89de08e48e26836d8c857191",
+    "url": "/booklice/404.html"
+  },
+  {
+    "revision": "75517a1de30d8ea3c0f9",
+    "url": "/booklice/css/app.5c9713c3.css"
+  },
+  {
+    "revision": "3bc73106326aa4dd1e0a",
+    "url": "/booklice/css/chunk-vendors.590a9ee0.css"
+  },
+  {
+    "revision": "492f0e7aa02b59131e82cbbb28c95012",
+    "url": "/booklice/index.html"
+  },
+  {
+    "revision": "d4dc81baa4ecebbfdfe3",
+    "url": "/booklice/js/about.4d875236.js"
+  },
+  {
+    "revision": "75517a1de30d8ea3c0f9",
+    "url": "/booklice/js/app.b90a1a04.js"
+  },
+  {
+    "revision": "38e8cd2640b6b55f8c96",
+    "url": "/booklice/js/bookmarks.4170fa89.js"
+  },
+  {
+    "revision": "5ce138f71bf530561b14",
+    "url": "/booklice/js/chunk-2bfef5ad.893b838c.js"
+  },
+  {
+    "revision": "720a4d4569ae66ef8157",
+    "url": "/booklice/js/chunk-2d0bceed.73a235cd.js"
+  },
+  {
+    "revision": "ed72c7163155b2b517dd",
+    "url": "/booklice/js/chunk-2d0d9fc1.4c4c9ec2.js"
+  },
+  {
+    "revision": "9014a42a3e43925ef993",
+    "url": "/booklice/js/chunk-2d21ecbc.104a4328.js"
+  },
+  {
+    "revision": "a1110e7e88696f62d334",
+    "url": "/booklice/js/chunk-2d21ed65.ff494edc.js"
+  },
+  {
+    "revision": "1502856fa5e368c84afa",
+    "url": "/booklice/js/chunk-392ccdba.3b95e4a9.js"
+  },
+  {
+    "revision": "07520820e8b83841005b",
+    "url": "/booklice/js/chunk-e592fad4.2c5f0625.js"
+  },
+  {
+    "revision": "3bc73106326aa4dd1e0a",
+    "url": "/booklice/js/chunk-vendors.1d5427ab.js"
+  },
+  {
+    "revision": "03c65e49d5e654a33e85e463ba35aa6c",
+    "url": "/booklice/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/booklice/robots.txt"
+  }
+]);
