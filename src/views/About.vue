fc9ea71923a4b0e91ab4861e7383eddc29ca6987
Scenaristeur <scenaristeur@gmail.com>
2021-07-03 14:06:09 +0200

    video about

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,5 +3,9 @@
     <h1>Bookmark on Pod for Alice = Booklice</h1>
 
     <a href="https://github.com/scenaristeur/booklice/" target="_blank">Booklice source</a>
+
+    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/f4uKb6EnDKs?start=2" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
   </div>
 </template>
