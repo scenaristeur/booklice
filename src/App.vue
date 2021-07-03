d01dca4a79d238048cd8de99afacdf0c25888882
Scenaristeur <scenaristeur@gmail.com>
2021-07-03 14:37:46 +0200

    video

--- src/App.vue
+++ src/App.vue
@@ -9,28 +9,40 @@
     <router-view/>
 
     <ul>
-      <li>Booklice te permet d'enregistrer tes liens favoris sur ton POD.</li>
-      <li>dans le dossier /public/bookmarks comme
-        <a href="https://spoggy-test13.solidcommunity.net/public/bookmarks/"
-        target="_blank">celui-ci</a>
-      </li>
-      <li>
-        te permettant ensuite de le partager avec le paramètre <b>?source=</b>
-      </li>
-      <li>
-        ce qui donne par exemple
-        <a href="https://scenaristeur.github.io/booklice?source=https://spoggy-test13.solidcommunity.net/public/bookmarks/"
+
+      <li v-if="path != null && path.length>0">
+        Pour partager tes booklices :
+        <a :href="'https://scenaristeur.github.io/booklice?source='+path"
         target="_blank">
-        https://scenaristeur.github.io/booklice?source=https://spoggy-test13.solidcommunity.net/public/bookmarks/
+        https://scenaristeur.github.io/booklice?source={{path}}
       </a>
+
+    </li>
+
+    <li>Booklice te permet d'enregistrer tes liens favoris sur ton POD.</li>
+    <li>dans le dossier /public/bookmarks comme
+      <a href="https://spoggy-test13.solidcommunity.net/public/bookmarks/"
+      target="_blank">celui-ci</a>
     </li>
     <li>
-      Et tu peux même, si tu le souhaites autoriser tes amis à ajouter des booklices et/ou les editer...
+      te permettant ensuite de le partager avec le paramètre <b>?source=</b>
     </li>
+    <li>
+      ce qui donne par exemple
+      <a href="https://scenaristeur.github.io/booklice?source=https://spoggy-test13.solidcommunity.net/public/bookmarks/"
+      target="_blank">
+      https://scenaristeur.github.io/booklice?source=https://spoggy-test13.solidcommunity.net/public/bookmarks/
+    </a>
+  </li>
 
-  </ul>
 
-  --
+  <li>
+    Et tu peux même, si tu le souhaites autoriser tes amis à ajouter des booklices et/ou les editer...
+  </li>
+
+</ul>
+
+--
 
 </div>
 </template>
@@ -45,32 +57,37 @@ export default {
   components: {
     'Login': () => import('@/components/solid/Login')
   },
-}
-</script>
+  computed:{
+    path:{
+      get() { return this.$store.state.booklice.path},
+      set(path) {this.$store.commit('booklice/setPath', path)}    }
+    },
+  }
+  </script>
 
-/* <style scoped>
-#app{
+  /* <style scoped>
+  #app{
     text-align: center;
-}
-</style>
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+  }
+  </style>
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
 
-#nav {
-  padding: 30px;
-}
+  #nav {
+    padding: 30px;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style> */
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
+  </style> */
--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,7 @@
       <h1>Bookmark on Pod for Alice = Booklice</h1>
 
       <a href="https://github.com/scenaristeur/booklice/" target="_blank">Booklice source</a>
-
+      <hr class="my-4">
       <iframe width="560" height="315" src="https://www.youtube.com/embed/yV1Z4Sfvebo" title="Booklice" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <!-- <iframe width="100%" height="400px" src="https://youtu.be/yV1Z4Sfvebo?t=10" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
 
