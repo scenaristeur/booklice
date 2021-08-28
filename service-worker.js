965f42a86254f55a87265fc57e3532ae8034564a
Scenaristeur <scenaristeur@gmail.com>
2021-08-28 14:33:45 +0200

    add https://trinpod.us as provider #7

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/booklice/precache-manifest.189c4cc755a6fde278ce706293ace2e5.js"
+  "/booklice/precache-manifest.19ceb44d8e7e8bf49290322e7d46ed99.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "booklice"});
