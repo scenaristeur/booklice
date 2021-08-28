75cfa33ca1182444fdcf94341f2bb81344ffbc54
Scenaristeur <scenaristeur@gmail.com>
2021-08-28 15:09:24 +0200

    3 min vid in about

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,6 +5,11 @@
 
       <a href="https://github.com/scenaristeur/booklice/" target="_blank">Booklice source</a>
       <hr class="my-4">
+<h3>Pod creation & Booklice use in 3min</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/_d1P09RshWY" title="Booklice" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+      <h3>Booklice utilisation et explication en français</h3>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/yV1Z4Sfvebo" title="Booklice" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <!-- <iframe width="100%" height="400px" src="https://youtu.be/yV1Z4Sfvebo?t=10" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
 
