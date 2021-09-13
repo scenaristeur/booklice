a1844cf903f7d3df49310e5748e342867493bf1b
Scenaristeur <scenrister@galcm>
2021-09-13 20:22:40 +0200

    merge visnetwork branch

---
+++ precache-manifest.4fa992096600b8a5d18bff9762f8c9a9.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b93e38a1348925b0483372af6acfff7",
+    "url": "/booklice/404.html"
+  },
+  {
+    "revision": "59e360f1841f50ce1b08237e92b66881",
+    "url": "/booklice/booklice_identity"
+  },
+  {
+    "revision": "2e2404e58af5883f120a",
+    "url": "/booklice/css/app.c11ca7bd.css"
+  },
+  {
+    "revision": "312e39cd943e313c0f50",
+    "url": "/booklice/css/chunk-47b0cc71.2fb9e275.css"
+  },
+  {
+    "revision": "7c32edba2dcccad5d02b",
+    "url": "/booklice/css/chunk-8c2f73b0.d1b4766b.css"
+  },
+  {
+    "revision": "f9921978893a5567cfb8",
+    "url": "/booklice/css/chunk-vendors.85f48eb3.css"
+  },
+  {
+    "revision": "a1a2d01bcd034270a3bc92176edfb66c",
+    "url": "/booklice/img/addNodeIcon.a1a2d01b.png"
+  },
+  {
+    "revision": "dd0baa69a69937600dc3bf035f3873c4",
+    "url": "/booklice/img/backIcon.dd0baa69.png"
+  },
+  {
+    "revision": "d5267b8db2498e44bd567bd12e07576b",
+    "url": "/booklice/img/connectIcon.d5267b8d.png"
+  },
+  {
+    "revision": "260c9c6535065674bb5e59a73f894abb",
+    "url": "/booklice/img/cross.260c9c65.png"
+  },
+  {
+    "revision": "02d321edc6e03aea1675001899d8f479",
+    "url": "/booklice/img/deleteIcon.02d321ed.png"
+  },
+  {
+    "revision": "bbfbf2e8eaa8ebbce8524769ac0a5bc0",
+    "url": "/booklice/img/downArrow.bbfbf2e8.png"
+  },
+  {
+    "revision": "d5422321c4f6feed4081891051f9a6b2",
+    "url": "/booklice/img/editIcon.d5422321.png"
+  },
+  {
+    "revision": "8965662a4348c548189b97493c21a19e",
+    "url": "/booklice/img/leftArrow.8965662a.png"
+  },
+  {
+    "revision": "5a379c46656d7a393c8f1ab293e46def",
+    "url": "/booklice/img/minus.5a379c46.png"
+  },
+  {
+    "revision": "4c4bf0a13eae5c7cdb109f91f8563f77",
+    "url": "/booklice/img/plus.4c4bf0a1.png"
+  },
+  {
+    "revision": "4f88ef172e7bc7e98e24fc7df5a05c4b",
+    "url": "/booklice/img/rightArrow.4f88ef17.png"
+  },
+  {
+    "revision": "f0aa0566b9f39334bd886d27d6a4cfe1",
+    "url": "/booklice/img/upArrow.f0aa0566.png"
+  },
+  {
+    "revision": "5f78e86f895f5a5cc1472cc57db154c5",
+    "url": "/booklice/img/zoomExtends.5f78e86f.png"
+  },
+  {
+    "revision": "2c19d4222555bd20a02a3b2d6bb8fc9f",
+    "url": "/booklice/index.html"
+  },
+  {
+    "revision": "71f357937151d583cfce",
+    "url": "/booklice/js/about.aba11da2.js"
+  },
+  {
+    "revision": "2e2404e58af5883f120a",
+    "url": "/booklice/js/app.f54e6943.js"
+  },
+  {
+    "revision": "38487efcf3d2c303a190",
+    "url": "/booklice/js/bookmarks.1b6583f1.js"
+  },
+  {
+    "revision": "0e33866b7c338ef28cca",
+    "url": "/booklice/js/chunk-2d0d9fc1.18e4533b.js"
+  },
+  {
+    "revision": "2a5f2c45ece696a96e33",
+    "url": "/booklice/js/chunk-2d0dd0fd.332f1e20.js"
+  },
+  {
+    "revision": "4f460eff3d49c68062a6",
+    "url": "/booklice/js/chunk-2d0e5782.e59ecdc6.js"
+  },
+  {
+    "revision": "350ed4dcbc61658dcfb6",
+    "url": "/booklice/js/chunk-2d217c19.64453a92.js"
+  },
+  {
+    "revision": "bbcc305dffefe774d913",
+    "url": "/booklice/js/chunk-2d21ed65.11fdeb8f.js"
+  },
+  {
+    "revision": "df2df74843e529c68f6c",
+    "url": "/booklice/js/chunk-392ccdba.c7917624.js"
+  },
+  {
+    "revision": "ad1c2dafe60d6267cb98",
+    "url": "/booklice/js/chunk-452d915a.60fb4e0e.js"
+  },
+  {
+    "revision": "312e39cd943e313c0f50",
+    "url": "/booklice/js/chunk-47b0cc71.69eab463.js"
+  },
+  {
+    "revision": "150035db99434ca678ae",
+    "url": "/booklice/js/chunk-7945f78c.0c16f96c.js"
+  },
+  {
+    "revision": "7c32edba2dcccad5d02b",
+    "url": "/booklice/js/chunk-8c2f73b0.2920df5e.js"
+  },
+  {
+    "revision": "07520820e8b83841005b",
+    "url": "/booklice/js/chunk-e592fad4.2c5f0625.js"
+  },
+  {
+    "revision": "f9921978893a5567cfb8",
+    "url": "/booklice/js/chunk-vendors.8724263d.js"
+  },
+  {
+    "revision": "03c65e49d5e654a33e85e463ba35aa6c",
+    "url": "/booklice/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/booklice/robots.txt"
+  }
+]);
